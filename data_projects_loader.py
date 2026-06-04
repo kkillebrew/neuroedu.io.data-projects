5da75d816d9a14f2eeb61ef53133af364b98c9ce
kkillebrew <kylekillabees@gmail.com>
2026-06-03 23:31:56 -0700

    updating family tree for lieber side

--- data_projects_loader.py
+++ data_projects_loader.py
@@ -44,7 +44,7 @@ def get_project_descriptions():
         },
         {
             "title": "Does more tech really increase test scores?",
-            "desc": "Data-driven evaluation of technological availability in learning environments and their effects on educational outcomes around the world.",
+            "desc": "Data-driven evaluation of technological availability in learning environments and their effects on education around the world.",
             "status": "active", 
             "page": "pages/4_tech_ed_app.py",
             "button_text": "Launch Tech in Education"
